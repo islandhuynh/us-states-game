0e8126fd696289bbbc839f21721d09a9be3aec13
Island <islandhuynh@gmail.com>
2021-06-23 01:10:24 -0400

    added logic to extract state x and y coordinates and plot name onto map using the turtle module

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas
 import turtle
 
+STYLE = ("Comic Sans", 12, "normal")
+
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
@@ -11,16 +13,20 @@ turtle.shape(image)
 states_data = pandas.read_csv("50_states.csv")
 states_list = states_data["state"].to_list()
 
+
 while len(states_list) > 0:
-  answer_state = screen.textinput(title=f"{50 - len(states_list)}/ 50", prompt="What's another state's name?")
+  answer_state = screen.textinput(title=f"{50 - len(states_list)}/50", prompt="What's another state's name?")
   if answer_state in states_list:
+    state_xcor = states_data[states_data["state"] == answer_state]["x"].to_list()[0]
+    state_ycor = states_data[states_data["state"] == answer_state]["y"].to_list()[0]
+    state_name = turtle.Turtle()
+    state_name.hideturtle()
+    state_name.penup()
+    state_name.goto(x=state_xcor, y=state_ycor)
+    state_name.write(answer_state, align="center")
+
     states_list.remove(answer_state)
   if answer_state == "stop":
     break
-
-def get_mouse_click_coor(x, y):
-  print(x, y)
-
-turtle.onscreenclick(get_mouse_click_coor)
 
 turtle.mainloop()
