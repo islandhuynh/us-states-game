052c9252ac01209c8f182f81e8199956b8ad5da9
Island <islandhuynh@gmail.com>
2021-06-23 01:16:42 -0400

    Added title casing to the user input

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import turtle
 STYLE = ("Comic Sans", 12, "normal")
 
 screen = turtle.Screen()
+screen.setup(width=1000, height=600)
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
@@ -15,7 +16,7 @@ states_list = states_data["state"].to_list()
 
 
 while len(states_list) > 0:
-  answer_state = screen.textinput(title=f"{50 - len(states_list)}/50", prompt="What's another state's name?")
+  answer_state = screen.textinput(title=f"{50 - len(states_list)}/50", prompt="What's another state's name?").title()
   if answer_state in states_list:
     state_xcor = states_data[states_data["state"] == answer_state]["x"].to_list()[0]
     state_ycor = states_data[states_data["state"] == answer_state]["y"].to_list()[0]
